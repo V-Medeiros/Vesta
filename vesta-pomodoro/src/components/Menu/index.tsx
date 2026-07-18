8ce07a5379faa980545cf904867f8bc4ccc9b6bc
Victor <victorme2806@gmail.com>
2026-07-18 02:42:12 -0300

    fix: remove unused theme state

--- vesta-pomodoro/src/components/Menu/index.tsx
+++ vesta-pomodoro/src/components/Menu/index.tsx
@@ -1,12 +1,7 @@
 import { HistoryIcon, MoonIcon, SettingsIcon } from 'lucide-react';
 import styles from './style.module.css';
-import { useState } from 'react';
-
-type availableThemes = 'dark' | 'light';
 
 export function Menu() {
-
-  const  [theme, setTheme]= useState<availableThemes>('dark') 
   return (
     <nav className={styles.menu} aria-label='Ações'>
       <button
