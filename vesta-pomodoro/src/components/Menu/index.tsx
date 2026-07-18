b2594c3a113b9428d03fee26020731b6cd8ac35c
Victor <victorme2806@gmail.com>
2026-07-18 05:25:49 -0300

    tema claro e escuro

--- vesta-pomodoro/src/components/Menu/index.tsx
+++ vesta-pomodoro/src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, MoonIcon, SettingsIcon } from 'lucide-react';
 import styles from './style.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Menu() {
   type availableType = 'dark' | 'light';
@@ -11,17 +11,18 @@ export function Menu() {
   ) {
     event.preventDefault();
 
-    console.log('clicado', Date.now())
-
     setTheme(prevTheme => {
-      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
-    } )
+    });
   }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
     <>
-    <h1>{theme}</h1>
       <nav className={styles.menu} aria-label='Ações'>
         <button
           className={`${styles.buttonMenu} ${styles.historyButton}`}
