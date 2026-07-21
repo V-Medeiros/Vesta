d048f865873fb0adc107c401fc96fcdcb71ab2c0
Victor <victorme2806@gmail.com>
2026-07-20 22:35:42 -0300

    state

--- vesta-pomodoro/src/App.tsx
+++ vesta-pomodoro/src/App.tsx
@@ -1,6 +1,22 @@
-
+import { useState } from 'react';
 import { Home } from './pages/Home';
+import type { TaskStateModel } from './Models/taskStateModule';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
+  const [state, setState] = useState<TaskStateModel>(initialState);
+
   return <Home />;
 }
--- vesta-pomodoro/src/pages/Home/index.tsx
+++ vesta-pomodoro/src/pages/Home/index.tsx
@@ -2,9 +2,15 @@ import { CountDown } from '../../components/CountDown';
 import { Cycle } from '../../components/Cycle';
 import { FocusFlame } from '../../components/FocusFlame';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../Models/taskStateModule';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+}
+
+export function Home(props: HomeProps) {
   return (
     <MainTemplate>
       <section className='timer-area' aria-labelledby='session-title'>
